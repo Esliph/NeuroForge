7ff718abd31a19c681d47e1397e04bbc5a30ce02
Dan Ruan <danrsaquetti@gmail.com>
2025-06-01 13:56:29 -0300

    feat: Creating Individual class with implementation of changes and crossover

---
+++ include/neuro/individual.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <vector>
+
+#include "neuro/activation.hpp"
+#include "neuro/layer.hpp"
+#include "neuro/neural_network.hpp"
+
+namespace neuro {
+
+class Individual {
+  NeuralNetwork neuralNetwork;
+  float fitness{};
+
+ public:
+  Individual() = default;
+  Individual(const Individual&) = default;
+  Individual(NeuralNetwork& neuralNetwork);
+  Individual(const std::vector<Layer>& layers);
+  Individual(const std::vector<int>& structure, const ActivationFunction& activation);
+  Individual(const std::vector<int>& structure, const std::vector<ActivationFunction>& activations);
+
+  inline void loadWeights(float range = 1.0f);
+  void loadWeights(float rangeMin, float rangeMax);
+
+  inline void loadBias(float range = 1.0f);
+  void loadBias(float rangeMin, float rangeMax);
+
+  void mutate(float rate, float strength);
+  Individual crossover(const Individual& partner) const;
+
+  Individual& operator=(const Individual&) = default;
+
+  const NeuralNetwork& getNeuralNetwork();
+  float getFitness();
+};
+
+};  // namespace neuro
---
+++ src/neuro/individual.cpp
@@ -0,0 +1,64 @@
+#include "neuro/individual.hpp"
+
+#include <random>
+#include <vector>
+
+#include "neuro/activation.hpp"
+#include "neuro/layer.hpp"
+#include "neuro/neural_network.hpp"
+
+namespace neuro {
+
+Individual::Individual(NeuralNetwork& neuralNetwork)
+    : neuralNetwork(neuralNetwork) {}
+
+Individual::Individual(const std::vector<Layer>& layers)
+    : neuralNetwork(layers) {}
+
+Individual::Individual(const std::vector<int>& structure, const ActivationFunction& activation)
+    : neuralNetwork(structure, activation) {}
+
+Individual::Individual(const std::vector<int>& structure, const std::vector<ActivationFunction>& activations)
+    : neuralNetwork(structure, activations) {}
+
+inline void Individual::loadWeights(float range) {
+  neuralNetwork.loadWeights(range);
+}
+
+void Individual::loadWeights(float rangeMin, float rangeMax) {
+  neuralNetwork.loadWeights(rangeMin, rangeMax);
+}
+
+inline void Individual::loadBias(float range) {
+  neuralNetwork.loadBias(range);
+}
+
+void Individual::loadBias(float rangeMin, float rangeMax) {
+  neuralNetwork.loadBias(rangeMin, rangeMax);
+}
+
+void Individual::mutate(float rate, float strength) {
+  std::random_device rd;
+  std::default_random_engine engine(rd());
+
+  neuralNetwork.mutate(rate, strength, engine);
+}
+
+Individual Individual::crossover(const Individual& partner) const {
+  std::random_device rd;
+  std::default_random_engine engine(rd());
+
+  auto newNeuralNetwork(neuralNetwork.crossover(partner.neuralNetwork, engine));
+
+  return Individual(newNeuralNetwork);
+}
+
+const NeuralNetwork& Individual::getNeuralNetwork() {
+  return neuralNetwork;
+}
+
+float Individual::getFitness() {
+  return fitness;
+}
+
+};  // namespace neuro
