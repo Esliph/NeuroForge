f3de4bba62209e2e0a58b58e701813476a16e016
Dan Ruan <danrsaquetti@gmail.com>
2025-06-04 22:18:23 -0300

    build: Building package with new interfaces

---
+++ packages/NeuroForge-1.0.0/include/neuro/interfaces/i_individual.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "neuro/interfaces/i_neural_network.hpp"
+
+namespace neuro {
+
+class IIndividual {
+ public:
+  IIndividual(IIndividual&) = default;
+  virtual ~IIndividual() = default;
+
+  virtual void evaluateFitness(const std::function<float(const INeuralNetwork&)>& evaluateFunction) = 0;
+
+  virtual void mutate(float rate, float intensity) = 0;
+  virtual IIndividual crossover(const IIndividual& partner) const = 0;
+
+  virtual void setNeuralNetwork(std::unique_ptr<INeuralNetwork>) = 0;
+  virtual float setFitness(float) = 0;
+
+  virtual const INeuralNetwork& getNeuralNetwork() const = 0;
+  virtual INeuralNetwork& getNeuralNetworkMutable() = 0;
+  virtual float getFitness() const = 0;
+
+  virtual IIndividual& operator=(IIndividual&) = default;
+};
+
+};  // namespace neuro
---
+++ packages/NeuroForge-1.0.0/include/neuro/interfaces/i_neural_network.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <vector>
+
+#include "neuro/interfaces/i_layer.hpp"
+
+namespace neuro {
+
+class INeuralNetwork {
+ public:
+  INeuralNetwork(INeuralNetwork&) = default;
+  virtual ~INeuralNetwork() = default;
+
+  virtual neuro_layer_t evaluate(const neuro_layer_t& input) = 0;
+
+  virtual void mutate(float rate, float intensity) = 0;
+  virtual ILayer crossover(const ILayer& partner) const = 0;
+
+  virtual void addLayer(std::unique_ptr<ILayer>) = 0;
+
+  virtual void clearLayers() = 0;
+
+  virtual void randomizeAll(float max = 1.0f) {
+    randomizeAll(-max, max, -max, max);
+  }
+
+  virtual void randomizeAll(float min, float max) {
+    randomizeAll(min, max, min, max);
+  }
+
+  virtual void randomizeAll(float minWeights, float maxWeights, float minBias, float maxBias) = 0;
+
+  virtual void randomizeBias(float max = 1.0f) {
+    randomizeBias(-max, max);
+  }
+
+  virtual void randomizeWeights(float max = 1.0f) {
+    randomizeWeights(-max, max);
+  }
+
+  virtual void randomizeBias(float min, float max) = 0;
+  virtual void randomizeWeights(float min, float max) = 0;
+
+  virtual void setAllWeights(const std::vector<layer_weight_t>&) = 0;
+  virtual void setAllBias(const std::vector<layer_bias_t>&) = 0;
+
+  virtual const neuro_layer_t& getLastOutput() const = 0;
+
+  virtual const std::shared_ptr<ILayer> getLayer(size_t index) const = 0;
+  virtual size_t getNumLayers() const = 0;
+
+  std::vector<ILayer>::const_iterator begin() const;
+  std::vector<ILayer>::const_iterator end() const;
+
+  std::vector<ILayer>::iterator begin();
+  std::vector<ILayer>::iterator end();
+
+  virtual INeuralNetwork& operator=(const INeuralNetwork&) = default;
+};
+
+};  // namespace neuro
