f93b0a942483929981d553da3065a5666402de01
Dan Ruan <danrsaquetti@gmail.com>
2025-06-05 21:38:34 -0300

    refactor: Refactoring `IPopulation` interface to segregate into base interface and `IGeneticPopulation`

--- include/neuro/interfaces/population/i_population.hpp
+++ include/neuro/interfaces/population/i_population.hpp
@@ -1,19 +1,24 @@
 #pragma once
 
+#include "neuro/common/i_iterable.hpp"
 #include "neuro/interfaces/i_individual.hpp"
 
 namespace neuro {
 
-class IPopulation {
+class IPopulation : IIterable<IIndividual> {
  public:
   IPopulation(IPopulation&) = default;
   virtual ~IPopulation() = default;
 
-  virtual void evaluate(const std::function<float(const INeuralNetwork&)>& evaluateFunction) = 0;
   virtual void evolve() = 0;
 
   virtual const std::vector<std::unique_ptr<IIndividual>>& getIndividuals() const = 0;
+  virtual std::vector<std::unique_ptr<IIndividual>>& getIndividualsMutable() = 0;
+
   virtual const IIndividual& getBestIndividual() const = 0;
+
+  virtual const IIndividual& get(size_t index) const = 0;
+  virtual IIndividual& get(size_t index) = 0;
 
   virtual float size() const = 0;
 };
