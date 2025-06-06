1d669cd6465a101cb383ecec671a263f97a40173
Dan Ruan <danrsaquetti@gmail.com>
2025-06-05 22:17:54 -0300

    feat: Creating main lib file that includes all main interfaces

---
+++ include/neuro/neuro.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "neuro/interfaces/i_individual.hpp"
+#include "neuro/interfaces/layer/i_layer.hpp"
+#include "neuro/interfaces/layer/i_stateful_layer.hpp"
+#include "neuro/interfaces/neural_network/i_neural_network.hpp"
+#include "neuro/interfaces/population/i_genetic_population.hpp"
+#include "neuro/interfaces/population/i_population.hpp"
+#include "neuro/interfaces/strategies/i_back_propagation_trainable.hpp"
+#include "neuro/interfaces/strategies/i_genetic_trainable.hpp"
+#include "neuro/interfaces/strategies/i_reinforcement_trainable.hpp"
+#include "neuro/utils/activation.hpp"
