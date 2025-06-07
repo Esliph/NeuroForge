27b8326cb9ed9c6eba36b5a89fc883686dfae21e
Dan Ruan <danrsaquetti@gmail.com>
2025-06-07 16:48:44 -0300

    feat: Creating complete implementation for the `DenseLayer` class

--- include/neuro/impl/dense_layer.hpp
+++ include/neuro/impl/dense_layer.hpp
@@ -20,6 +20,33 @@ class DenseLayer : public ILayer {
   DenseLayer(const layer_weight_t& weights, const layer_bias_t& biases, ActivationFunction& activation);
 
   virtual ~DenseLayer() = default;
+
+  neuro_layer_t feedforward(const neuro_layer_t& inputs) override;
+
+  void randomizeWeights(float min, float max) override;
+  void randomizeBiases(float min, float max) override;
+
+  void setActivationFunction(const ActivationFunction&) override;
+
+  void setWeights(const layer_weight_t&) override;
+  void setBiases(const layer_bias_t&) override;
+
+  void setWeight(int indexX, int indexY, float value) override;
+  void setBias(int index, float value) override;
+
+  const layer_weight_t& getWeights() const override;
+  const layer_bias_t& getBiases() const override;
+
+  layer_weight_t& getWeights() override;
+  layer_bias_t& getBiases() override;
+
+  const ActivationFunction& getActivationFunction() const override;
+
+  float getWeight(int indexX, int indexY) const override;
+  float getBias(int index) const override;
+
+  size_t inputSize() const override;
+  size_t outputSize() const override;
 };
 
 };  // namespace neuro
---
+++ src/neuro/impl/dense_layer.cpp
@@ -0,0 +1,118 @@
+#include "neuro/impl/dense_layer.hpp"
+
+#include <random>
+
+#include "neuro/interfaces/layer/i_layer.hpp"
+#include "neuro/utils/random_engine.hpp"
+
+namespace neuro {
+
+DenseLayer::DenseLayer(int inputSize, int outputSize, ActivationFunction& activation)
+    : ILayer(),
+      weights(outputSize, neuro_layer_t(inputSize)),
+      biases(outputSize),
+      activation(activation) {}
+
+DenseLayer::DenseLayer(const layer_weight_t& weights, const layer_bias_t& biases, ActivationFunction& activation)
+    : ILayer(),
+      weights(weights),
+      biases(biases),
+      activation(activation) {}
+
+DenseLayer::DenseLayer(const layer_weight_t& weights, ActivationFunction& activation)
+    : ILayer(),
+      weights(weights),
+      biases(weights.size()),
+      activation(activation) {}
+
+neuro_layer_t DenseLayer::feedforward(const neuro_layer_t& inputs) {
+  neuro_layer_t outputs(biases.size());
+
+  for (size_t i = 0; i < outputs.size(); i++) {
+    float total = biases[i];
+
+    for (size_t j = 0; j < inputs.size(); j++) {
+      total += weights[i][j] * inputs[j];
+    }
+
+    outputs[i] = activation.activate(total);
+  }
+
+  return outputs;
+}
+
+void DenseLayer::randomizeWeights(float min, float max) {
+  std::uniform_real_distribution<float> dist(min, max);
+
+  for (auto& neurons : weights) {
+    for (auto& weight : neurons) {
+      weight = dist(random_engine);
+    }
+  }
+}
+
+void DenseLayer::randomizeBiases(float min, float max) {
+  std::uniform_real_distribution<float> dist(min, max);
+
+  for (auto& bias : biases) {
+    bias = dist(random_engine);
+  }
+}
+
+void DenseLayer::setActivationFunction(const ActivationFunction& activation) {
+  this->activation = activation;
+}
+
+void DenseLayer::setWeights(const layer_weight_t& weights) {
+  this->weights = weights;
+}
+
+void DenseLayer::setBiases(const layer_bias_t& biases) {
+  this->biases = biases;
+}
+
+void DenseLayer::setWeight(int indexX, int indexY, float value) {
+  weights[indexX][indexY] = value;
+}
+
+void DenseLayer::setBias(int index, float value) {
+  biases[index] = value;
+}
+
+const layer_weight_t& DenseLayer::getWeights() const {
+  return weights;
+}
+
+const layer_bias_t& DenseLayer::getBiases() const {
+  return biases;
+}
+
+layer_weight_t& DenseLayer::getWeights() {
+  return weights;
+}
+
+layer_bias_t& DenseLayer::getBiases() {
+  return biases;
+}
+
+float DenseLayer::getWeight(int indexX, int indexY) const {
+  return weights[indexX][indexY];
+}
+
+float DenseLayer::getBias(int index) const {
+  return biases[index];
+}
+
+const ActivationFunction& DenseLayer::getActivationFunction() const {
+  return activation;
+}
+
+size_t DenseLayer::inputSize() const {
+  return weights.empty() ? 0 : weights[0].size();
+}
+
+size_t DenseLayer::outputSize() const {
+  return weights.size();
+}
+
+};  // namespace neuro
