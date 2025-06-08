ba1fb05663bc21f5641c692a1a25fbc744ae25df
Dan Ruan <danrsaquetti@gmail.com>
2025-06-08 18:35:08 -0300

    remove: Removing reference to deleted files in "interfaces.hpp" header

--- include/neuro/interfaces/interfaces.hpp
+++ include/neuro/interfaces/interfaces.hpp
@@ -4,7 +4,3 @@
 #include "neuro/interfaces/i_layer.hpp"
 #include "neuro/interfaces/i_neural_network.hpp"
 #include "neuro/interfaces/i_population.hpp"
-#include "neuro/interfaces/i_stateful_layer.hpp"
-#include "neuro/interfaces/strategies/i_back_propagation_trainable.hpp"
-#include "neuro/interfaces/strategies/i_genetic_trainable.hpp"
-#include "neuro/interfaces/strategies/i_reinforcement_trainable.hpp"
