ab0bedad033944412026c431b793d7d066f479e3
Dan Ruan <danrsaquetti@gmail.com>
2025-06-08 18:33:56 -0300

    Removing the evolve method and making corrections to the semantics of the methods and adding operators to the `IPopulation` interface

--- include/neuro/interfaces/i_population.hpp
+++ include/neuro/interfaces/i_population.hpp
@@ -12,17 +12,18 @@ class IPopulation {
   IPopulation(const IPopulation&) = default;
   virtual ~IPopulation() = default;
 
-  virtual void evolve() = 0;
-
   virtual const std::vector<std::unique_ptr<IIndividual>>& getIndividuals() const = 0;
-  virtual std::vector<std::unique_ptr<IIndividual>>& getIndividualsMutable() = 0;
+  virtual std::vector<std::unique_ptr<IIndividual>>& getIndividuals() = 0;
 
   virtual const IIndividual& getBestIndividual() const = 0;
 
   virtual const IIndividual& get(size_t index) const = 0;
   virtual IIndividual& get(size_t index) = 0;
 
-  virtual float size() const = 0;
+  virtual size_t size() const = 0;
+
+  virtual std::unique_ptr<IPopulation> clone() const = 0;
+  virtual IPopulation& operator=(const IPopulation&) = default;
 };
 
 };  // namespace neuro
