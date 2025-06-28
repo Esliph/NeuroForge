352cdacd748dc1de35eab16ea9f1f83c2a90d1e5
Dan Ruan <danrsaquetti@gmail.com>
2025-06-28 11:36:54 -0300

    refactor: Moving "make" the `ActivationFunction` statements to the new `neuro::maker` namespace

---
+++ include/neuro/makers/activation.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <cmath>
+#include <functional>
+
+#include "neuro/utils/activation.hpp"
+
+namespace neuro {
+
+  namespace maker {
+
+    inline ActivationFunction makeSigmoid() {
+      return {
+          [](float x) { return 1.0f / (1.0f + std::exp(-x)); },
+          [](float y) { return y * (1.0f - y); }};
+    }
+
+    inline ActivationFunction makeRelu() {
+      return {
+          [](float x) { return x > 0 ? x : 0.0f; },
+          [](float y) { return y > 0 ? 1.0f : 0.0f; }};
+    }
+
+    inline ActivationFunction makeTanh_fn() {
+      return {
+          [](float x) { return std::tanh(x); },
+          [](float y) { return 1.0f - y * y; }};
+    }
+
+    inline ActivationFunction makeLeaky_relu() {
+      return {
+          [](float x) { return x > 0 ? x : 0.01f * x; },
+          [](float y) { return y > 0 ? 1.0f : 0.01f; }};
+    }
+
+    inline ActivationFunction makeElu() {
+      return {
+          [](float x) { return x >= 0 ? x : std::exp(x) - 1.0f; },
+          [](float y) { return y >= 0 ? 1.0f : y + 1.0f; }};
+    }
+
+    inline ActivationFunction makeSwish() {
+      return {
+          [](float x) { return x / (1.0f + std::exp(-x)); },
+          [](float y) { return y + (1.0f - y) * y; }};
+    }
+
+    inline ActivationFunction makeSoftplus() {
+      return {
+          [](float x) { return std::log1p(std::exp(x)); },
+          [](float y) { return 1.0f - std::exp(-y); }};
+    }
+
+    inline ActivationFunction makeHard_sigmoid() {
+      return {
+          [](float x) { return std::max(0.0f, std::min(1.0f, 0.2f * x + 0.5f)); },
+          [](float y) { return (y > 0.0f && y < 1.0f) ? 0.2f : 0.0f; }};
+    }
+
+  };  // namespace maker
+
+};  // namespace neuro
--- include/neuro/neuro.hpp
+++ include/neuro/neuro.hpp
@@ -3,5 +3,6 @@
 #include "neuro/exceptions/exceptions.hpp"
 #include "neuro/impl/impl.hpp"
 #include "neuro/interfaces/interfaces.hpp"
+#include "neuro/makers/makers.hpp"
 #include "neuro/strategies/strategies.hpp"
 #include "neuro/utils/utils.hpp"
