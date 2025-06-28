c6098cf83b7b2de619d22ba577080b224226f5cf
Dan Ruan <danrsaquetti@gmail.com>
2025-06-28 11:29:44 -0300

    feat: Creating exception related to network architecture

---
+++ include/neuro/exceptions/invalid_network_architecture_exception.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+
+#include "neuro/exceptions/exception.hpp"
+
+namespace neuro {
+
+  namespace exception {
+
+    class InvalidNetworkArchitectureException : public NeuroException {
+     public:
+      explicit InvalidNetworkArchitectureException(const std::string& message);
+    };
+
+  };  // namespace exception
+
+};  // namespace neuro
---
+++ src/neuro/exceptions/invalid_network_architecture_exception.cpp
@@ -0,0 +1,16 @@
+#include "neuro/exceptions/invalid_network_architecture_exception.hpp"
+
+#include <string>
+
+#include "neuro/exceptions/exception.hpp"
+
+namespace neuro {
+
+  namespace exception {
+
+    explicit InvalidNetworkArchitectureException::InvalidNetworkArchitectureException(const std::string& message)
+        : NeuroException("Invalid Network Architecture: " + message) {}
+
+  };  // namespace exception
+
+};  // namespace neuro
