73960d71528e8f63f345849f453f00cf1be6068a
Dan Ruan <danrsaquetti@gmail.com>
2025-06-28 11:29:15 -0300

    feat: Creating base exception from `neuro` library

---
+++ include/neuro/exceptions/exception.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <exception>
+#include <string>
+
+namespace neuro {
+
+  namespace exception {
+
+    class NeuroException : public std::exception {
+     protected:
+      std::string message;
+
+     public:
+      explicit NeuroException(const std::string& message);
+
+      const char* what() const noexcept override;
+    };
+
+  };  // namespace exception
+
+};  // namespace neuro
---
+++ src/neuro/exceptions/exception.cpp
@@ -0,0 +1,18 @@
+#include "neuro/exceptions/exception.hpp"
+
+#include <exception>
+#include <string>
+
+namespace neuro {
+
+  namespace exception {
+
+    NeuroException::NeuroException(const std::string& message) : message(message) {}
+
+    const char* NeuroException::what() const noexcept {
+      return message.c_str();
+    }
+
+  };  // namespace exception
+
+};  // namespace neuro
