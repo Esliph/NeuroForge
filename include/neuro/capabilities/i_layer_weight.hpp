11b85fc7577c17b84a44cc792aa144e982aed93f
Dan Ruan <danrsaquetti@gmail.com>
2025-08-23 15:36:12 -0300

    refactor: Segregating the `ILayer` interface by moving the weights and biases methods to the `ILayerWeight` interface

---
+++ include/neuro/capabilities/i_layer_weight.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "neuro/types.hpp"
+
+namespace neuro {
+
+  class ILayerWeight {
+   public:
+    virtual ~ILayerWeight() = default;
+
+    virtual void randomizeWeights(float min, float max) = 0;
+    virtual void randomizeBiases(float min, float max) = 0;
+
+    virtual float meanWeight() const = 0;
+    virtual float meanBias() const = 0;
+
+    virtual float& weightRef(size_t indexX, size_t indexY) = 0;
+    virtual float& biasRef(size_t index) = 0;
+
+    virtual const float& weightRef(size_t indexX, size_t indexY) const = 0;
+    virtual const float& biasRef(size_t index) const = 0;
+
+    virtual float getWeight(size_t indexX, size_t indexY) const = 0;
+    virtual float getBias(size_t index) const = 0;
+
+    virtual void setWeight(size_t indexX, size_t indexY, float value) = 0;
+    virtual void setBias(size_t index, float value) = 0;
+
+    virtual layer_weight_t& getWeights() = 0;
+    virtual layer_bias_t& getBiases() = 0;
+
+    virtual const layer_weight_t& getWeights() const = 0;
+    virtual const layer_bias_t& getBiases() const = 0;
+
+    virtual void setWeights(const layer_weight_t&) = 0;
+    virtual void setBiases(const layer_bias_t&) = 0;
+  };
+
+}; // namespace neuro
--- include/neuro/interfaces/i_layer.hpp
+++ include/neuro/interfaces/i_layer.hpp
@@ -1,49 +1,20 @@
 #pragma once
 
 #include <memory>
-#include <random>
-#include <vector>
 
 #include "neuro/capabilities/i_layer_operation.hpp"
 #include "neuro/capabilities/i_layer_structure.hpp"
-#include "neuro/types.hpp"
-#include "neuro/utils/activation.hpp"
+#include "neuro/capabilities/i_layer_weight.hpp"
 
 namespace neuro {
 
   class ILayer
     : public ILayerOperation,
-      public ILayerStructure {
+      public ILayerStructure,
+      public ILayerWeight {
    public:
     ILayer() = default;
     virtual ~ILayer() = default;
-
-    virtual void randomizeWeights(float min, float max) = 0;
-    virtual void randomizeBiases(float min, float max) = 0;
-
-    virtual float meanWeight() const = 0;
-    virtual float meanBias() const = 0;
-
-    virtual float& weightRef(size_t indexX, size_t indexY) = 0;
-    virtual float& biasRef(size_t index) = 0;
-
-    virtual const float& weightRef(size_t indexX, size_t indexY) const = 0;
-    virtual const float& biasRef(size_t index) const = 0;
-
-    virtual float getWeight(size_t indexX, size_t indexY) const = 0;
-    virtual float getBias(size_t index) const = 0;
-
-    virtual void setWeight(size_t indexX, size_t indexY, float value) = 0;
-    virtual void setBias(size_t index, float value) = 0;
-
-    virtual layer_weight_t& getWeights() = 0;
-    virtual layer_bias_t& getBiases() = 0;
-
-    virtual const layer_weight_t& getWeights() const = 0;
-    virtual const layer_bias_t& getBiases() const = 0;
-
-    virtual void setWeights(const layer_weight_t&) = 0;
-    virtual void setBiases(const layer_bias_t&) = 0;
 
     virtual std::unique_ptr<ILayer> clone() const = 0;
 
