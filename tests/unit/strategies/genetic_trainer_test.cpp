766d3902b375814cbeabb4f79ed49af4d84e515c
Dan Ruan <danrsaquetti@gmail.com>
2025-08-24 15:23:24 -0300

    feat: Adding tests to validate changing the parameters of the `GeneticOptions` options in `GeneticTrainer`

--- tests/unit/strategies/genetic_trainer_test.cpp
+++ tests/unit/strategies/genetic_trainer_test.cpp
@@ -10,7 +10,36 @@
 #include "neuro/interfaces/i_population.hpp"
 #include "neuro/makers/activation.hpp"
 
-TEST_CASE("Testing changes in weights and biases through mutation") {
+TEST_CASE("GeneticTrainer - Testing changes to option parameters") {
+  std::vector<int> structure = {1, 1};
+  std::shared_ptr<neuro::IPopulation> population = std::make_shared<neuro::Population>(2, structure);
+
+  const neuro::GeneticOptions defaultOptions{};
+
+  neuro::GeneticTrainer trainer(population);
+
+  REQUIRE(trainer.getOptions() == defaultOptions);
+
+  SUBCASE("Rate change tests") {
+    trainer.setRate(0.75f);
+
+    CHECK(trainer.getOptions().rate == doctest::Approx(0.75f));
+  }
+
+  SUBCASE("Intensity change tests") {
+    trainer.setIntensity(0.25f);
+
+    CHECK(trainer.getOptions().intensity == doctest::Approx(0.25f));
+  }
+
+  SUBCASE("Elite Count change tests") {
+    trainer.setEliteCount(10);
+
+    CHECK(trainer.getOptions().eliteCount == 10);
+  }
+}
+
+TEST_CASE("GeneticTrainer - Testing changes in weights and biases through mutation") {
   std::vector<int> structure = {1, 1};
   std::shared_ptr<neuro::IPopulation> population = std::make_shared<neuro::Population>(2, structure);
 
